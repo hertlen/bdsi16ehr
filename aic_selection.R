06c768eb604e562949485f050a34c57ecb33184c
Gabriel Goulart <gabrielgoulart@0587343722.wireless.umich.net>
2016-07-08 16:15:58 -0400

    k

--- aic_selection.R
+++ aic_selection.R
@@ -1,6 +1,6 @@
 library(survey)
 
-load("NHANES.RData")
+data0 = read.csv("NHANES.csv")
 
 
 data1 <- data0[data0$SDDSRVYR >= 2 & data0$SDDSRVYR <= 8,]
@@ -14,14 +14,17 @@ NHANES.MEC.design1 <- svydesign(
   data = data1
 )
 
-covariates = c("hypertension","age_years","diabetes",
-               "BMI","Total_chol")
+covariates = c("hypertension","age_years","male","diabetes",
+               "BMI","Total_chol","factor(Smoking)","factor(race_eth)")
+
+svyglm(CKD_epi_eGFR ~ +hypertension+age_years+diabetes+
+         BMI+Total_chol+factor(Smoking), design = NHANES.MEC.design1)
 
 aic.old = 10^6
-list.aic = rep(0,5)
+list.aic = rep(0,length(covariates))
 for (i in 1:length(covariates)){
-  fm = as.formula(paste("CKD_epi_eGFR ~",covariates[i]))
-  fit1a = svyglm(fm, design = NHANES.MEC.design1)
+  fm = as.formula(paste("CKD ~",covariates[i]))
+  fit1a = svyglm(fm, design = NHANES.MEC.design1, family="binomial")
   aic.new = fit1a$aic
   list.aic[i] = aic.new
   if (aic.new < aic.old){
@@ -39,31 +42,37 @@ while(done==0){
   previous.aic = best.aic
   list.aic = NULL
   list.cov.aic = NULL
+  loop.count = 0
   for (i in 1:length(covariates)){
-    if(i %in% best.is) {next # skip 3rd iteration and go to next iteration
+    if(i %in% best.is) {
+      loop.count = loop.count+1
+      next # skip iteration and go to next iteration
       cat(n) }
     fm = as.formula(paste(deparse(best.fm),"+",covariates[i]))
-    fit1a = svyglm(fm, design = NHANES.MEC.design1)
+    fit1a = svyglm(fm, design = NHANES.MEC.design1, family = "binomial")
     aic.new = fit1a$aic
     list.aic = c(list.aic,aic.new)
     list.cov.aic = c(list.cov.aic,covariates[i])
   }
-  
-  best.aic = which(list.aic == min(list.aic))
-  best.fm = as.formula(paste(deparse(best.fm),"+",list.cov.aic[best.aic]))
-  best.is = c(best.is,which(covariates == list.cov.aic[best.aic]))
-  aic.history = c(aic.history,min(list.aic))
-  if(previous.aic == best.aic){
+  if (loop.count != length(covariates)){
+    best.i = which(list.aic == min(list.aic))
+    best.aic = min(list.aic)
+    best.fm = as.formula(paste(deparse(best.fm),"+",list.cov.aic[best.i]))
+    best.is = c(best.is,which(covariates == list.cov.aic[best.i]))
+    aic.history = c(aic.history,min(list.aic))
+  }
+  if(previous.aic < best.aic | loop.count == length(covariates) ){
     done = 1
   }
 }
 
+fit1a = svyglm(best.fm, design = NHANES.MEC.design1, family="binomial")
+summary(fit1a)
 
 
 
 
 
 
-
-
+data[[whatever]]
 
