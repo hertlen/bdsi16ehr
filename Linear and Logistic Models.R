84ae57a8358f04e11e0f6c31dcef0e6a6753cf45
Nicholas Hertle <nhertle@0587341321.wireless.umich.net>
2016-07-08 15:22:44 -0400

    add parts to cross-validation function

--- Linear and Logistic Models.R
+++ Linear and Logistic Models.R
@@ -25,12 +25,23 @@ temp.svd = svydesign(
 # races : 1 = mexican american, 2 = other hispanic, 3 = non-hispanic white, 4 = non-hispanic black,
 # 5 = other
 
+
+
 model_ckd1 = svyglm(CKD_epi_eGFR ~ age_years + BMI + Triglycerides, design = temp.svd)
 
 model_ckd2 = svyglm(factor(CKD) ~ age_years + hypertension + BMI + Triglycerides
                    + Total_chol, design = temp.svd, family = quasibinomial)
 
 model_ckd3 = svyglm(factor(CKD) ~ age_years, design = temp.svd, family = quasibinomial)
+
+model_ckd4 = svyglm(factor(CKD) ~ age_years + smoking + hypertension + BMI, design = temp.svd,
+                    family = quasibinomial)
+
+# Predictions and Graphing ####
+# to create predictions for GLMs:
+# prediction = predict(model_name, newdata = data.frame(covariates = data.frame()))
+# to plot
+# lines(prediction)
 
 # CKD is a binary variable for CKD_stage > 0
 
@@ -40,27 +51,15 @@ quantile(data0$BMI, probs = c(0.025, 0.975), na.rm = TRUE) # 95% of people have 
 (range(data0[!is.na(data0$Triglycerides), ]$Triglycerides)) # Triglycerides ranges from 14-5210
 quantile(data0$Triglycerides, probs = c(0.025, 0.975), na.rm = TRUE) # 95% of people have 36-394
 
-svyboxplot(BMI ~ factor(hypertension),
+
+# box plot for factor data
+svyboxplot(CKD ~ factor(hypertension),
            design = temp.svd,
            all.outliers = TRUE,
            axes = FALSE,
            xlab = "Hypertension",
-           ylab = "BMI",
+           ylab = "CKD",
            col = "yellow",
            border = "lightblue")
 axis(1, at = c(1,2), labels = c("No", "Yes"))
 axis(2)
-
-# Total_chol and diabetes are highly correlated
-# (high coefficient) with years - atp_chol less so
-
-model2 = svyglm(Total_chol ~ age_years, design = temp.svd)
-model3 = svyglm(atp_chol ~ age_years, design = temp.svd)
-model4 = svyglm(diabetes ~ age_years, design = temp.svd, family = quasibinomial(link = "logit"))
-diab.predict = predict(model4, newdata = data.frame(age_years = 2:100))
-lines(diab.predict)
-
-prediction = predict(model, newdata = data.frame(age_years = 2:100))
-
-svyplot(CKD_epi_eGFR~age_years, design = temp.svd, pch = 19)
-lines(prediction)
--- cross-validation svy.R
+++ cross-validation svy.R
@@ -1,8 +1,7 @@
 # Arguments: data frame, family (default gaussian),
 # equation (Response ~ covariate1 + covariate2...)
 
-
-cross-validation = function(data, family = gaussian(), equation) {
+cross_validation = function(data, type.fam = gaussian(), response_var, covariates) {
   data_sub = data[data$SDDSRVYR >= 2 & data$SDDSRVYR <= 8, ]
   weight = data_sub$WTMEC2YR / 7
   survey_design = svydesign(
@@ -12,18 +11,30 @@ cross-validation = function(data, family = gaussian(), equation) {
     weights = ~weight,
     data = data_sub
   )
+  # build equation from response, covariates
+  equation = paste(response_var, "~", covariates[1])
+  if(length(covariates) > 1) {
+    for(i in 2:length(covariates)) {
+      equation = paste(equation, "+", covariates[i], sep = " ")
+    }
+  }
   iterator = seq(from = 1, to = nrow(data_sub), by = 1)
   survey_design$variables = cbind(iterator, survey_design$variables)
+  errors = numeric(length(iterator))
   for(i in 1:length(iterator)){
-    # Check and make sure this works correctly:
     # leave out the ith row, indicated by iterator
     current.design = subset(survey_design, survey_design$variables$iterator != i)
     survey_glm = svyglm(
+      # reshape arguments into GLM equation
       as.formula(equation),
       design = current.design, 
       family = type.fam
     )
-    
+    print(survey_design[survey_design$variables$iterator == i, ])
+    # probably need to have covariates passed/parsed as individual arguments
+    # prediction = predict(survey_glm, newdata = survey_design[survey_design$variables$iterator == i, ]$)
+    # save squared errors
+    # errors[i] = (prediction - survey_design[survey_design$variables$iterator == i, ]$response_var) ^ 2
   }
 }
 
