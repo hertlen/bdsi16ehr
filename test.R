94ac7821dd5c506ace45e0078aa4d7cf77321599
Nicholas Hertle <nhertle@0587396459.wireless.umich.net>
2016-07-16 21:50:45 -0400

    added age group

--- test.R
+++ test.R
@@ -7,6 +7,9 @@ source("k-fold cross-validation.R")
 
 data0 = read.csv("NHANES.csv")
 data0 = data0[data0$age_years >= 18, ]
+agegrp2 = as.numeric((data0$age_years >=40 & data0$age_years<=59))
+agegrp3 = as.numeric((data0$age_years >= 60))
+data0 = cbind(data0, agegrp2, agegrp3)
 
 smoking_former = as.numeric((data0$Smoking == "Former Smoker"))
 smoking_current = as.numeric((data0$Smoking == "Current Smoker"))
@@ -24,43 +27,10 @@ education5 = as.numeric((data0$education == 5))
 
 data0 = cbind(data0, education2, education3, education4, education5)
 
-covariates_test = c("hypertension", "diabetes", "BMI", "smoking_former", "smoking_current",
-                    "LDL", "age_years")
-## eGFR model:
-# 33.8% valid observations (13,500)
-# 11.89 error, SE 0.116
-# BIC: 8657
-#####################
-covariates_test = c("hypertension", "male", "diabetes", "education2", "education3", "education4", "education5",
-                    "Triglycerides", "age_months", "private_ins")
-## eGFR model:
-# 36% valid observations (14600)
-# 12.23 error, SE 0.12
-# BIC: 10755
-#######################
-covariates_test = c("hypertension", "male", "diabetes", "LDL", "HDL", "Total_chol",
-                    "Triglycerides", "age_months", "private_ins")
-## eGFR model:
-# 20.7% valid observations (8300)
-# 11.86 error, SE 0.134
-# BIC: 5703
-#######################
+covariates_test = c("hypertension", "male", "diabetes", "race2", "race3", "race4", "race5",
+                    "agegrp2", "agegrp3")
 
-covariates_test = c("hypertension", "male", "diabetes", "education2", "education3", "education4", "education5",
-             "LDL", "age_months", "private_ins")
-## eGFR model:
-# 19% valid observations (7667)
-# 11.8 error, 0.148 SE
-# BIC: 5542
 
-#######################
-covariates_test = c("hypertension", "male", "diabetes", "LDL", "age_months", "private_ins")
-## eGFR model:
-# 20.7% valid observations(8306)
-# 11.89 error, 0.1364 SE
-# BIC: 5699
-
-#####################
 race2 = as.numeric((data0$race_eth == 2))
 race3 = as.numeric((data0$race_eth == 3))
 race4 = as.numeric((data0$race_eth == 4))
@@ -68,10 +38,41 @@ race5 = as.numeric((data0$race_eth == 5))
 
 data0 = cbind(data0, race2, race3, race4, race5)
 
-covariates_test = c("hypertension", "male", "diabetes", "age_months", "race2",
-                    "race3", "race4", "race5")
-## eGFR model:
-# % valid observations
+
+weight = data0$WTMEC2YR / 7
+survey_design = svydesign(
+  ids = ~SDMVPSU,
+  strata = ~SDMVSTRA,
+  nest = TRUE,
+  weights = ~weight,
+  data = data0
+)
+
+chosen_glm = svyglm(
+  CKD_epi_eGFR ~ hypertension + diabetes + agegrp2 + agegrp3 + male + race2 + race3 + race4 + 
+    race5,
+  design = survey_design,
+  family = gaussian
+)
+
+###
+covariates_test = c("hypertension", "male", "diabetes", "race2", "race3", "race4", "race5",
+                    "agegrp2", "agegrp3", "Total_chol")
+
+# 85% observations used (34320)
+#     mean     SE
+# error 12.936 0.0763
+
+
+###
+
+covariates_test = c("hypertension", "male", "diabetes", "race2", "race3", "race4", "race5",
+                    "agegrp2", "agegrp3")
+# 85% observations used (34332)
+#         mean     SE
+# error 12.916 0.0768
+
+
 
 test = k_fold_cross_validate(data0, binomial = FALSE, covariates = covariates_test, num_folds = 5,
                              first.year = 2, last.year = 8)
@@ -107,3 +108,56 @@ weighted_errors = svymean(
 )
 
 weighted_errors
+
+
+
+
+
+
+
+##########################################################################################
+
+
+covariates_test = c("hypertension", "diabetes", "BMI", "smoking_former", "smoking_current",
+                    "LDL", "age_years")
+## eGFR model:
+# 33.8% valid observations (13,500)
+# 11.89 error, SE 0.116
+# BIC: 8657
+#####################
+covariates_test = c("hypertension", "male", "diabetes", "education2", "education3", "education4", "education5",
+                    "Triglycerides", "age_months", "private_ins")
+## eGFR model:
+# 36% valid observations (14600)
+# 12.23 error, SE 0.12
+# BIC: 10755
+#######################
+covariates_test = c("hypertension", "male", "diabetes", "LDL", "HDL", "Total_chol",
+                    "Triglycerides", "age_months", "private_ins")
+## eGFR model:
+# 20.7% valid observations (8300)
+# 11.86 error, SE 0.134
+# BIC: 5703
+#######################
+
+covariates_test = c("hypertension", "male", "diabetes", "education2", "education3", "education4", "education5",
+                    "LDL", "age_months", "private_ins")
+## eGFR model:
+# 19% valid observations (7667)
+# 11.8 error, 0.148 SE
+# BIC: 5542
+
+#######################
+covariates_test = c("hypertension", "male", "diabetes", "LDL", "age_months", "private_ins")
+## eGFR model:
+# 20.7% valid observations(8306)
+# 11.89 error, 0.1364 SE
+# BIC: 5699
+
+#####################
+covariates_test = c("hypertension", "male", "diabetes", "agegrp2", "agegrp3", "race2",
+                    "race3", "race4", "race5")
+## eGFR model:
+# 58% valid observations
+# 11.936 error, 0.085 SE
+# BIC: 5296132
